4d1815be06090e1fbad9e29737ad8c2ad1b0a6a9
Dartherick PI <ericktejadacompres@gmail.com>
2022-03-15 11:38:10 -0400

    nothing important

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import dronekit
 import time
 
 #Connects to the vehicle using serial port.
-Drone = dronekit.connect('/dev/ttyS0', wait_ready=True, baud=921600)
-#Drone = dronekit.connect('/dev/ttyAMA0', wait_ready=True, baud=921600)
+#Drone = dronekit.connect('/dev/ttyS0', wait_ready=True, baud=921600)
+Drone = dronekit.connect('/dev/ttyAMA0', wait_ready=True, baud=921600)
 
 if (Drone.armed):
     Drone.armed   = False
