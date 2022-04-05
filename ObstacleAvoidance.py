6bed62ed0839946e16f6579025083fee06488337
Erick Tejada (Eaton) <ericktejadacompres@gmail.com>
2022-04-05 16:52:51 -0400

    Updating Measure function

--- ObstacleAvoidance.py
+++ ObstacleAvoidance.py
@@ -1,36 +1,31 @@
+'''
+This script converts the distance measured by any sensor and converts it into a distance_sensor mavlink message and sends it to the FC via the telemetry port. 
+For more info http://ardupilot.org/dev/docs/code-overview-object-avoidance.html The function sensor_data(d,o) encodes distance and orientation as mavlink message 
+and sends them to the fc. The argument distance takes values in cm and orientation takes value as following: 
+0:Forward 1:Forward-Right 2:Right 3:Back-Right 4:Back 5:Back-Left 6:Left 7:Forward-Left 24:Up 25:Down 
+For addition of more sensors, the distance can be collected and sent via the sensor_data function with the correct orientation.
+'''
+
 import dronekit
-import RPi.GPIO as GPIO
 import gpiozero
 import time
 
-#{TRIG,ECHO}
-FrontSensor = {4,17}
-BackSensor = {27,22}
-RightSensor = {10,9}
-LeftSensor = {11,5}
+ECHO_PINS = {"Front":17,"Back":22,"Right":9,"Left":5}
+TRIGGER_PINS = {"Front":4,"Back":27,"Right":10,"Left":11}
 
-#
 Orientation = {0:"Forward", 1:"Forward-Right", 2:"Right", 3:"Back-Right", 4:"Back", 5:"Back-Left", 6:"Left", 7:"Forward-Left" ,24:"Up", 25:"Down"}
 
-GPIO.setmode(GPIO.BCM)
+FrontSensor = gpiozero.DistanceSensor(ECHO_PINS["Front"],TRIGGER_PINS["Front"])
+BackSensor = gpiozero.DistanceSensor(ECHO_PINS["Back"],TRIGGER_PINS["Back"])
+RightSensor = gpiozero.DistanceSensor(ECHO_PINS["Right"],TRIGGER_PINS["Right"])
+LeftSensor = gpiozero.DistanceSensor(ECHO_PINS["Left"],TRIGGER_PINS["Left"])
 
-GPIO.setup(FrontSensor[0],GPIO.OUT)
-GPIO.setup(FrontSensor[1],GPIO.IN)
-
-GPIO.setup(BackSensor[0],GPIO.OUT)
-GPIO.setup(BackSensor[1],GPIO.IN)
-
-GPIO.setup(LeftSensor[0],GPIO.OUT)
-GPIO.setup(LeftSensor[1],GPIO.IN)
-
-GPIO.setup(RightSensor[0],GPIO.OUT)
-GPIO.setup(RightSensor[1],GPIO.IN)
 
 #Connects to the vehicle using serial port.
 vehicle = dronekit.connect('/dev/ttyS0', wait_ready=True, baud=57600)
 
 #Function to convert distance and orientation into a mavlink message
-def sensor_data(d,o):
+def SensorData(d,o):
     msg=vehicle.message_factory.distance_sensor_encode(
         0,
         5,
@@ -44,35 +39,13 @@ def sensor_data(d,o):
     vehicle.send_mavlink(msg)
 
 #Simple function to measure the distance using ultrasonic sensor
-def measure(Sensor):
-    dist1 = 250
-    GPIO.output(Sensor[0], True)
-    time.sleep(0.00001)
-    GPIO.output(Sensor[0], False)
-    echo_state = 0
-	
-    while echo_state == 0:
-        echo_state = GPIO.input(Sensor[0])
-        pulse_start = time.time()
-			
-    while GPIO.input(Sensor[0])==1:
-        pulse_end = time.time()
-			
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
-	
-    if(distance < 250 and distance > 5): #To filter out junk values
-        dist1 = distance
-			
-    sensor_data(dist1,0) #Sends measured distance(dist1) and orientation(0) as a mavlink message.
-    return dist1
+def Measure():
+    Sensors = {"Front","Back","Right","Left"}
+
+    for i in Sensors:
+        d = gpiozero.DistanceSensor(ECHO_PINS[i],TRIGGER_PINS[i]).distance()
+        SensorData(d,)
+
 
 # Main code
 #if __Name__ == __main__:
-while True:
-    d = measure()
-    time.sleep(0.1)
-    print(d)
-
-print('temp')
