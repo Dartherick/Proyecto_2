79511f934c9388ad1d846667daf3e843867b282e
= <=>
2022-10-12 06:44:10 -0400

    Add evasion.py

--- Evasion.py
+++ Evasion.py
@@ -10,8 +10,7 @@ import dronekit
 import gpiozero
 import time
 
-#TRIGGER_PINS = {"Front":17,"Back":22,"Right":9,"Left":6}
-#ECHO_PINS = {"Front":27,"Back":10,"Right":0,"Left":13}
+import argparse
 
 TRIGGER_PINS = {"Front":6,"Back":17,"Right":9,"Left":22}
 ECHO_PINS = {"Front":13,"Back":27,"Right":0,"Left":10}
@@ -19,24 +18,31 @@ ECHO_PINS = {"Front":13,"Back":27,"Right":0,"Left":10}
 Orientation = {"Front":0, "Front-Right":1 ,"Right":2 ,"Back-Right":3 ,"Back":4 ,"Back-Left":5 ,"Left":6 ,"Front-Left":7, "Up":24 ,"Down":25}
 
 FrontSensor = gpiozero.DistanceSensor(ECHO_PINS["Front"],TRIGGER_PINS["Front"])
-BackSensor = gpiozero.DistanceSensor(ECHO_PINS["Back"],TRIGGER_PINS["Back"])
 RightSensor = gpiozero.DistanceSensor(ECHO_PINS["Right"],TRIGGER_PINS["Right"])
 LeftSensor = gpiozero.DistanceSensor(ECHO_PINS["Left"],TRIGGER_PINS["Left"])
 
-Sensors = {"Front":FrontSensor, "Back":BackSensor, "Right":RightSensor, "Left":LeftSensor}
-
+Sensors = {"Front":FrontSensor,"Right":RightSensor,"Left":LeftSensor}
 
 #Connects to the vehicle using serial port.
-vehicle = dronekit.connect('/dev/serial0', wait_ready=True, baud=921600)
+#vehicle = dronekit.connect('/dev/serial0', wait_ready=False, baud=921600)
+
+parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
+parser.add_argument('--connect', 
+                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+
+vehicle = dronekit.connect("/dev/serial0",wait_ready=True,baud=921600)
 
 #Function to convert distance and orientation into a mavlink message
 def SensorData(d,o):
     msg=vehicle.message_factory.distance_sensor_encode(
         0,          # Time since system boot (ignored)
         5,          # Min distance cm
-        250,        # Max distance cm
+        500,        # Max distance cm
         int(d),     # Current distance, must be int
-        0,          # Type of sensor (ignored)
+        1,          # Type of sensor (ignored)
         0,          # Onboard id (ignored)
         o,          # Orientation
         0,          # Covariance (ignored)
@@ -44,13 +50,20 @@ def SensorData(d,o):
     vehicle.send_mavlink(msg)
 
 #Simple function to measure the distance using ultrasonic sensor
-def Measure():
-    SensorsName = {"Front","Back","Right","Left"}
+def Measure(K,t):
+    SensorsName = {"Front","Right","Left"}
 
     for i in SensorsName:
-        Distance = Sensors[i].distance * 100
-        #SensorData(Distance,Orientation[i])
-        print(f"{i} = {Distance} cm")
-
-while True:
-    Measure()
+        Distance = Sensors[i].distance * 100 * K
+        SensorData(Distance,Orientation[i])
+        print(f"{i} = {Distance} cm - {Distance/100} m")
+        time.sleep(t)
+    
+try:
+    while True:
+        Measure(5,0.05)
+        
+    vehicle.close()
+except KeyboardInterrupt:
+    vehicle.close()
+    print("Cerrando conexion...")
